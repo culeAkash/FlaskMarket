f5159bfadf4dce282c564135e20ee5d46c11b19b
Akash Jaiswal <akashjais929@gmail.com>
2023-08-17 11:14:07 +0530

    Learnt more about auth in flask

--- market/routes.py
+++ market/routes.py
@@ -3,7 +3,7 @@ from flask import render_template,redirect,url_for,flash
 from market.models import Item,User
 from market.forms import RegisterForm,LoginForm
 from market import db
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 
 
 @app.route("/")
@@ -12,6 +12,7 @@ def home_page():
     return render_template('home.html')
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
@@ -25,6 +26,8 @@ def register_page():
                               password=form.password.data)
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f'Account created successfully! You are not logged in as {user_to_create.user_name}',category='success')
         return redirect(url_for('market_page'));
     
     if form.errors!={}:
@@ -48,6 +51,12 @@ def login_page():
             flash('Username or password incorrect',category='danger');
               
     return render_template('login.html',form=form)
+
+@app.route("/logout")
+def logout_page():
+    logout_user()
+    flash("You have logged out!!!",category='info')
+    return redirect(url_for('home_page'));
 
 
 
